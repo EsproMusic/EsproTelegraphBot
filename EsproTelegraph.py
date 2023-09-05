0c87d6b842a8dbd41b5e18df10a2c6ed71374cfe
Espro Music <143268188+EsproMusic@users.noreply.github.com>
2023-09-05 14:55:10 +0530

    Espro Telegraph

--- EsproTelegraph.py
+++ EsproTelegraph.py
@@ -23,7 +23,7 @@ To generate links in **group chats**, add me to your supergroup and send the com
 
 🏠 | [Home](https://t.me/Esproupdate)
             """
-   reply_markup=InlineKeyboardMarkup(
+    reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
@@ -41,6 +41,7 @@ To generate links in **group chats**, add me to your supergroup and send the com
                 ]
            ]
         )
+    
     await EsproTelegraph.send_message(message.chat.id, text, disable_web_page_preview=True)
     
 
